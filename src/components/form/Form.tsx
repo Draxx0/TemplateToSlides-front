aed95848f86f68cc111b4643b3f2c8d996354600
Edgar <erajau@icloud.com>
2023-10-19 16:27:26 +0200

    :construction: formUI wip

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,25 +1,70 @@
 import { Card, CardBody, Heading } from "@chakra-ui/react";
 import ChooseTemplateInput from "./ChooseTemplateInput";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { PresentationData } from "../../types/presentation";
 import SlideTabs from "./SlideTabs";
 
 const Form = () => {
- const [presentationData, setPresentationData] = useState<PresentationData | null>(null)
+  const [presentationData, setPresentationData] = useState<PresentationData | null>();
+  const [isESDSelected, setIsESDSelected] = useState(false);
 
- return (
-  <Card maxWidth={"50%"}>
-   <CardBody display={"flex"} flexDirection={"column"} gap={6}>
-    <Heading size={"lg"}>Formulaire de création de slides</Heading>
+  const handleTemplateChange = (templateId) => {
+    setIsESDSelected(templateId === "ESD");
+  };
 
-    <ChooseTemplateInput presentationData={presentationData} setPresentationData={setPresentationData} />
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        position: "relative",
+        overflow: "hidden",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundImage: `url('aperture-vintage-HF1IO60tQdc-unsplash.jpg')`,
+          backgroundPosition: "center center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          filter: "opacity(0.5)",
+        }}
+      ></div>
+      <Card maxHeight={presentationData?.templateId ? 3000 : 200} maxWidth={"50%"} overflow={"hidden"} transition={"ease-in-out 0.5s all"}
+        style={{
+          zIndex: 1,
+          backgroundColor: "white",
+          padding: "20px",
+          borderRadius: "20px",
+          marginTop: isESDSelected ? "20px" : "0",
+          overflow: "hidden",
+          maxHeight: isESDSelected ? "80vh" : "auto",
+          transition: "max-height 3s ease-out",
+        }}
+      >
+        <CardBody display={"flex"} flexDirection={"column"} gap={6}>
+          <Heading size={"lg"}>Formulaire de création de slides</Heading>
 
-    {presentationData?.templateId && (
-     <SlideTabs presentationData={presentationData} setPresentationData={setPresentationData} />
-    )}
-   </CardBody>
-  </Card>
- );
-}
+          <ChooseTemplateInput
+            presentationData={presentationData}
+            setPresentationData={setPresentationData}
+            onTemplateChange={handleTemplateChange}
+          />
+
+          {presentationData?.templateId && (
+            <SlideTabs presentationData={presentationData} setPresentationData={presentationData} />
+          )}
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
 
 export default Form;
