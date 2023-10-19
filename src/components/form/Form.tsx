08e7c029f794045f53986148b3bb58bea227c374
DraxX0 <williamfort.lmgl@gmail.com>
2023-10-19 16:56:19 +0200

    :sparkles: :lipstick: UI done

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,70 +1,52 @@
-import { Card, CardBody, Heading } from "@chakra-ui/react";
+import { Box, Card, CardBody, Heading } from "@chakra-ui/react";
 import ChooseTemplateInput from "./ChooseTemplateInput";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { PresentationData } from "../../types/presentation";
 import SlideTabs from "./SlideTabs";
 
 const Form = () => {
-  const [presentationData, setPresentationData] = useState<PresentationData | null>();
-  const [isESDSelected, setIsESDSelected] = useState(false);
+ const [presentationData, setPresentationData] = useState<PresentationData | null>(null);
 
-  const handleTemplateChange = (templateId) => {
-    setIsESDSelected(templateId === "ESD");
-  };
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "100vh",
-        position: "relative",
-        overflow: "hidden",
-      }}
-    >
-      <div
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          backgroundImage: `url('aperture-vintage-HF1IO60tQdc-unsplash.jpg')`,
-          backgroundPosition: "center center",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          filter: "opacity(0.5)",
-        }}
-      ></div>
-      <Card maxHeight={presentationData?.templateId ? 3000 : 200} maxWidth={"50%"} overflow={"hidden"} transition={"ease-in-out 0.5s all"}
-        style={{
-          zIndex: 1,
-          backgroundColor: "white",
-          padding: "20px",
-          borderRadius: "20px",
-          marginTop: isESDSelected ? "20px" : "0",
-          overflow: "hidden",
-          maxHeight: isESDSelected ? "80vh" : "auto",
-          transition: "max-height 3s ease-out",
-        }}
-      >
-        <CardBody display={"flex"} flexDirection={"column"} gap={6}>
-          <Heading size={"lg"}>Formulaire de création de slides</Heading>
+ useEffect(() => {
+  console.log("ID :", presentationData?.templateId, "is", !!presentationData?.templateId)
+ }, [presentationData?.templateId])
+ return (
+  <Box
+   style={{
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "100vh",
+    position: "relative",
+    overflow: "hidden",
+   }}
+  >
+   <Box
+    position={"absolute"} inset={0} zIndex={0} backgroundImage={`url('./assets/bg.jpg')`} backgroundPosition={"center"} backgroundRepeat={"no-repeat"} backgroundSize={"cover"}
+   ></Box>
+   <Box position={"absolute"} inset={0} opacity={0.3} w={"100%"} h={"100%"} backgroundColor={"black"} zIndex={1}></Box>
+   <Card minW={"80%"} overflow={"hidden"} zIndex={2} borderRadius={20}
+    style={{
+     transition: "min-height 0.4s ease-in-out, max-height 1s ease-in-out",
+     maxHeight: presentationData?.templateId ? "2000px" : "200px",
+     minHeight: presentationData?.templateId ? "600px" : "200px"
+    }}>
+    <CardBody display={"flex"} flexDirection={"column"} gap={6}>
+     <Heading size={"lg"}>Formulaire de création de slides</Heading>
 
-          <ChooseTemplateInput
-            presentationData={presentationData}
-            setPresentationData={setPresentationData}
-            onTemplateChange={handleTemplateChange}
-          />
+     <ChooseTemplateInput
+      presentationData={presentationData}
+      setPresentationData={setPresentationData}
+     />
 
-          {presentationData?.templateId && (
-            <SlideTabs presentationData={presentationData} setPresentationData={presentationData} />
-          )}
-        </CardBody>
-      </Card>
-    </div>
-  );
+     {presentationData?.templateId && (
+      <SlideTabs presentationData={presentationData} setPresentationData={setPresentationData} />
+     )}
+    </CardBody>
+   </Card>
+  </Box >
+ );
 };
 
 export default Form;
